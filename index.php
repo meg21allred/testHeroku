ec4167d75db4c28fbb2e64f6800f3d1d2f5ce8cb
meganallred <meg21allred@gmail.com>
2020-09-27 21:45:06 -0400

    adding breaks

--- index.php
+++ index.php
@@ -10,26 +10,26 @@
     <form action="formGuy.php" method="post">
     
         <!-- name -->
-        <input type="text" name="name" placeholder="name">
+        <input type="text" name="name" placeholder="name"><br>
         
         <!-- email -->
-        <input type="text" name="email" placeholder="email">
+        <input type="text" name="email" placeholder="email"><br>
 
         <!-- radio buttons -->
         <input type="radio" name="major" id="comScience" value="Computer Science">
-        <label for="comScience">Computer Science</label>
+        <label for="comScience">Computer Science</label><br>
 
         <input type="radio" name="major" id="webDev" value="Web Design and Developement">
-        <label for="webDev">Web Design and Developement</label>
+        <label for="webDev">Web Design and Developement</label><br>
 
         <input type="radio" name="major" id="comInfo" value="Computer Information Technology">
-        <label for="comInfo">Computer Information Technology</label>
+        <label for="comInfo">Computer Information Technology</label><br>
 
         <input type="radio" name="major" id="comEngine" value="Computer Engineering">
-        <label for="comEngine">Computer Engineering</label>
+        <label for="comEngine">Computer Engineering</label><br>
 
         <!-- comments -->
-        <input type="textarea" name="comments" rows="5" cols="40" placeholder="comments...">
+        <input type="textarea" name="comments" rows="5" cols="40" placeholder="comments..."><br><br>
     
         <!-- submit button -->
         <button type="submit"></button>
