de585904c443ad22e5f14e0721f31a5cd6047d5d
meganallred <meg21allred@gmail.com>
2020-09-27 21:32:58 -0400

    adding labels

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
     <form action="formGuy.php" method="post">
     <input type="text" name="name" placeholder="name">
     <input type="text" name="email" placeholder="email">
+    <label for="major">Computer Science</label>
     <input type="radio" name="major" value="Computer Science">
+    <label for="major">Web Design and Developement</label>
     <input type="radio" name="major" value="Web Design and Developement">
     <input type="radio" name="major" value="Computer information technology">
     <input type="radio" name="major" value="Computer Engineering">
