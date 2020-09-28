7da4e13fe034d34dd5fafc0c323ab8b303b44d82
meganallred <meg21allred@gmail.com>
2020-09-27 21:37:21 -0400

    more labels

--- index.php
+++ index.php
@@ -9,12 +9,14 @@
     <form action="formGuy.php" method="post">
     <input type="text" name="name" placeholder="name">
     <input type="text" name="email" placeholder="email">
-    <label for="major">Computer Science</label>
-    <input type="radio" name="major" value="Computer Science">
-    <label for="major">Web Design and Developement</label>
-    <input type="radio" name="major" value="Web Design and Developement">
-    <input type="radio" name="major" value="Computer information technology">
-    <input type="radio" name="major" value="Computer Engineering">
+    <label for="comScience">Computer Science</label>
+    <input type="radio" name="major" id="comScience" value="Computer Science">
+    <label for="webDev">Web Design and Developement</label>
+    <input type="radio" name="major" id="webDev" value="Web Design and Developement">
+    <label for="comInfo">Computer Science</label>
+    <input type="radio" name="major" id="comInfo" value="Computer information technology">
+    <label for="comEngine">Computer Science</label>
+    <input type="radio" name="major" id="comEngine" value="Computer Engineering">
     <input type="textarea" name="comments" rows="5" cols="40" placeholder="comments...">
     <button type="submit"></button>
     </form>
