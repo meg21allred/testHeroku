b7c5234c0cf670935e8f661aee69652e747fa9da
meganallred <meg21allred@gmail.com>
2020-09-29 08:49:30 -0400

    testing radio again

--- formGuy.php
+++ formGuy.php
@@ -5,19 +5,8 @@ $userEmail = $_POST['email'];
 $userMajor = $_POST['major'];
 $userComments = $_POST['comments'];
 
-if (empty($_POST["major"])) {
-   
-    $gender = test_input($_POST["gender"]);
- }
-
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-
 echo "your info:".'<br><br>';
 echo "$userName".'<br>';
 echo '<a href="mailto:'.$userEmail.'">'.$userEmail.'</a>';
+echo $userMajor;
 ?>
