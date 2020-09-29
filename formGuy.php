9d8a12a2fcd9510a302dd6864145582a564d3e9d
meganallred <meg21allred@gmail.com>
2020-09-29 08:30:27 -0400

    form varibles

--- formGuy.php
+++ formGuy.php
@@ -1,3 +1,11 @@
 <?php
-echo "form guy!"
+echo "form guy!";
+
+$userName = $_POST['name'];
+$userEmail = $_POST['email'];
+$userMajor = $_POST['major'];
+$userComments = $_POST['comments'];
+
+echo "your info".'<br>';
+echo "$userName".'<br>';
 ?>
