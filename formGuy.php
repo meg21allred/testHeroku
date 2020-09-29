c8bb15e69661dd36666330d70276a0471c74f255
meganallred <meg21allred@gmail.com>
2020-09-29 10:36:22 -0400

    stretching goals

--- formGuy.php
+++ formGuy.php
@@ -10,16 +10,21 @@ echo "$userName".'<br>';
 echo '<a href="mailto:'.$userEmail.'">'.$userEmail.'</a>'.'<br>';
 echo $userMajor."<br>";
 
-$aContinents = $_POST['continents'];
+ $aContinents = $_POST['continents'];
  
-    $N = count($aContinents);
+//     $N = count($aContinents);
 
-    echo("You selected $N continents(s): "."<br>");
-    for($i=0; $i < $N; $i++)
-    {
-      echo($aContinents[$i] . "<br>");
-    }
+//     echo("You selected $N continents(s): "."<br>");
+//     for($i=0; $i < $N; $i++)
+//     {
+//       echo($aContinents[$i] . "<br>");
+//     }
   
+foreach ($aContinents as $place)
+{
+	$place_clean = htmlspecialchars($place);
+	echo "<p>$place_clean</p><br>";
+}
 
 echo $userComments;
 
