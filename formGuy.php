ef621ce3bbeac222b61f3b428850180c4bf7a092
meganallred <meg21allred@gmail.com>
2020-09-29 08:45:00 -0400

    testing radio buttons

--- formGuy.php
+++ formGuy.php
@@ -5,7 +5,19 @@ $userEmail = $_POST['email'];
 $userMajor = $_POST['major'];
 $userComments = $_POST['comments'];
 
-echo "your info".'<br>';
+if (empty($_POST["major"])) {
+   
+    $gender = test_input($_POST["gender"]);
+ }
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+
+echo "your info:".'<br><br>';
 echo "$userName".'<br>';
 echo '<a href="mailto:'.$userEmail.'">'.$userEmail.'</a>';
 ?>
